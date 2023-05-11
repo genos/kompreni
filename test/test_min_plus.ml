f8d73bf07f0c5e97cea73210cd65305fa0558775
Graham Enos <genos@rigetti.com>
2023-05-11 10:20:12 -0400

    Use Zarith for MinPlus

--- test/test_min_plus.ml
+++ test/test_min_plus.ml
@@ -1,11 +1,11 @@
 open Scaffolding
 
 module MinPlus = struct
-  type t = Finite of float | Infinite
+  type t = Finite of Q.t | Infinite
 
   let ( +& ) a b =
     match (a, b) with
-    | Finite x, Finite y -> Finite (Float.max x y)
+    | Finite x, Finite y -> Finite (Q.max x y)
     | Finite _, _ -> a
     | _, Finite _ -> b
     | _, _ -> Infinite
@@ -13,18 +13,20 @@ module MinPlus = struct
   let zero = Infinite
 
   let ( *& ) a b =
-    match (a, b) with Finite x, Finite y -> Finite (x +. y) | _, _ -> Infinite
+    match (a, b) with
+    | Finite x, Finite y -> Finite (Q.add x y)
+    | _, _ -> Infinite
 
-  let one = Finite 0.0
+  let one = Finite Q.zero
 
   let gen =
     QCheck2.Gen.frequency
       [
         (1, QCheck2.Gen.pure Infinite);
-        (1, QCheck2.Gen.map (fun x -> Finite x) QCheck2.Gen.float);
+        (1, QCheck2.Gen.map (fun x -> Finite (Q.of_float x)) QCheck2.Gen.float);
       ]
 end
 
 let () =
   let module MP = SemiringLaws (MinPlus) (MinPlus) in
-  Alcotest.run "Laws MinPlus Examples" [ ("MinPlus", MP.tests) ]
+  Alcotest.run "MinPlus Semiring Laws" [ ("MinPlus", MP.tests) ]
